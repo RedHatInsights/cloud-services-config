13e947aa7b7d2728bddcb701a651cf3f7d924e31
Asa Price <aprice@redhat.com>
2020-06-26 13:47:55 -0400

    Need one more creds file

--- Jenkinsfile
+++ Jenkinsfile
@@ -60,6 +60,7 @@ node {
           file(credentialsId: "rhcs-openshift-mirror-origin-json", variable: 'OPENSHIFTORIGINMIRRORJSON'),
           file(credentialsId: "rhcs-rhorchata-origin-json", variable: 'RHORCHATAORIGINJSON'),
           file(credentialsId: "rhcs-pentest-gateway-origin-json", variable: 'PENTESTGATEWAYORIGINJSON'),
+          file(credentialsId: "rhcs-gateway-origin-v4-json", variable: 'GATEWAYORIGINV4JSON'),
           string(credentialsId: "rhcs-$ENVSTR-gateway-secret", variable: 'GATEWAYSECRET'),
           string(credentialsId: "rhcs-pentest-gateway-secret", variable: 'PENTESTGATEWAYSECRET'),
           string(credentialsId: "rhcs-prod-certauth-secret", variable: 'CERTAUTHSECRET')
--- akamai/update_api.py
+++ akamai/update_api.py
@@ -99,7 +99,7 @@ def updatePropertyRulesUsingConfig(version_number, master_config_list, crc_env =
         ("<<rhorchata-origin-json>>", util.readFileAsString(util.getEnvVar("RHORCHATAORIGINJSON")),
         ("<<gateway-origin-json>>", util.readFileAsString(util.getEnvVar("GATEWAYORIGINJSON")),
         ("<<pentest-gateway-origin-json>>", util.readFileAsString(util.getEnvVar("PENTESTGATEWAYORIGINJSON")),
-        ("<<gateway-origin-json-v4>>", util.readFileAsString(util.getEnvVar("GATEWAYORIGINJSONV4")),
+        ("<<gateway-origin-v4-json>>", util.readFileAsString(util.getEnvVar("GATEWAYORIGINV4JSON")),
         ("<<openshift-origin-json>>", util.readFileAsString(util.getEnvVar("OPENSHIFTORIGINJSON")),
         ("<<openshift-origin-mirror-json>>", util.readFileAsString(util.getEnvVar("OPENSHIFTORIGINMIRRORJSON")))
     ]
