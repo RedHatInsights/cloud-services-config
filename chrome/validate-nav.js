88c0c568480b7193cadc792af09c419e37ebce06
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2021-07-21 11:57:15 +0200

    Use new chrome schema (prod-stable) (#738)
    * Use new navigation schemas
    
    * Temporarily disable the backup
    
    * Clean validate.yaml
    
    * Sync new schemas
    
    * Remove My namespaces from automation-hub
    
    * Sync the ansible bundle

---
+++ chrome/validate-nav.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const landingSchema = require('./validationSchemas/landing');
+const modulesSchema = require('./validationSchemas/modules');
+const navigationSchema = require('./validationSchemas/navigation');
+
+
+
+const navigationFiles = fs.readdirSync(__dirname)
+  .filter(f => f.endsWith('-navigation.json') && f !== 'landing-navigation.json');
+const landingFile = path.resolve(__dirname, 'landing-navigation.json')
+const modulesFile = path.resolve(__dirname, 'fed-modules.json')
+
+
+async function validateLanding() {    
+  try {
+    const file = fs.readFileSync(landingFile, 'utf-8')
+    await landingSchema.strict(true).validateAsync(JSON.parse(file));
+  } catch (error) {
+    console.error(`Error in landing page navigation definition.\n`, error)
+    process.exit(1)
+  }
+}
+
+async function validateModules() {
+  try {
+    const file = fs.readFileSync(modulesFile, 'utf-8')
+    await modulesSchema.strict(true).validateAsync(JSON.parse(file));
+  } catch (error) {
+    console.error(`Error in federated modules definition.\n`, error)
+    process.exit(1)
+  }
+}
+
+async function validateNavigation() {
+  navigationFiles.forEach(async fileName => {
+    const file = fs.readFileSync(path.join(__dirname, fileName), 'utf-8')
+    try {
+      const result = await navigationSchema.strict(true).validateAsync(JSON.parse(file));
+    } catch (error) {
+      console.error(`Error in ${fileName} navigation definition.\n`, error)
+      process.exit(1)
+    }
+  })
+}
+
+validateLanding()
+validateModules()
+validateNavigation()
