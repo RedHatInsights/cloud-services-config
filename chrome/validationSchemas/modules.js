08f8b1682e4d144c8561d9531d125beae8a6f77a
Martin Maroši <marvusm.mmi@gmail.com>
2021-09-23 16:53:19 +0200

    Add missing fedramp flags. (#914)

--- chrome/validationSchemas/modules.js
+++ chrome/validationSchemas/modules.js
@@ -20,7 +20,8 @@ const routeModuleSchema = Joi.object({
     is: Joi.array().has(Joi.object({
       pathname: Joi.any(),
       exact: Joi.any(),
-      dynamic: Joi.alternatives().try(Joi.boolean().valid(true), Joi.forbidden())
+      dynamic: Joi.alternatives().try(Joi.boolean().valid(true), Joi.forbidden()),
+      isFedramp: Joi.boolean(),
     })),
     then: Joi.required(),
   }),
@@ -28,11 +29,13 @@ const routeModuleSchema = Joi.object({
     pathname: Joi.string().required(),
     exact: Joi.boolean(),
     dynamic: Joi.boolean(),
+    isFedramp: Joi.boolean(),
   }))).required()
 })
 
 const moduleItemSchema = Joi.object({
   dynamic: Joi.boolean().optional(),
+  isFedramp: Joi.boolean(),
   manifestLocation: Joi.alternatives().conditional('dynamic', {
     is: false,
     then: Joi.any().forbidden(),
