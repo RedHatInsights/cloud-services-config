ea962150450eec865b573cc406fcd91d923230a5
Martin Maroši <marvusm.mmi@gmail.com>
2021-10-15 16:34:04 +0200

    [qa-stable] Enable travis checks (#937)
    * Enable chrome schema validation in qa-stable
    
    * Fix traling commans in application-services navigation schema.

--- chrome/validationSchemas/modules.js
+++ chrome/validationSchemas/modules.js
@@ -39,9 +39,10 @@ const moduleItemSchema = Joi.object({
     then: Joi.any().forbidden(),
     otherwise: Joi.string().required()
   }),
+  isFedramp: Joi.boolean(),
   modules: Joi.array().items(routeModuleSchema).required()
 })
 
-const modulesSchema = Joi.object().pattern(Joi.string(), moduleItemSchema);
+const modulesSchema = Joi.object().pattern(Joi.string().token(), moduleItemSchema);
 
 module.exports = modulesSchema;
